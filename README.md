ea868a2b347edb4350c994b7fcdd3336c112b97b
Mohit Talniya <mohittalniya@gmail.com>
2023-02-10 12:10:15 +0530

    updated the dependencies

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # CDK App to deploy a simple Atlas instance
 A cdk app to deploy a simple MongoDB Atlas instance
 
-### Scaffold using projen
+### This project has been created using projen
+Check below link for installation and configuration of projen
 
 https://github.com/projen/projen
 
 `npx projen new awscdk-app-ts`
 
-This will create a project structure for cdk apps.
+This will create a project structure for cdk apps. 
+
+## Deploy the app
+
+`export MONGODB_ATLAS_PUBLIC_KEY=<>`
+`export MONGODB_ATLAS_PRIVATE_KEY=<reducted>`
+`export MONGODB_ATLAS_ORG_ID=<reducted>`
+`npx cdk deploy`
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,52 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {ApiKeyDefinition, AtlasBasic} from "@mongodbatlas-awscdk/atlas-basic";
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    const orgId = this.node.tryGetContext('MONGODB_ATLAS_ORG_ID') || process.env.MONGODB_ATLAS_ORG_ID;
+    const apiKeys: ApiKeyDefinition = {
+      privateKey: this.node.tryGetContext('MONGODB_ATLAS_PRIVATE_KEY') || process.env.MONGODB_ATLAS_PRIVATE_KEY,
+      publicKey: this.node.tryGetContext('MONGODB_ATLAS_PUBLIC_KEY') || process.env.MONGODB_ATLAS_PUBLIC_KEY,
+    };
+
+    new AtlasBasic(this, 'atlas-basic', {
+      apiKeys: apiKeys,
+      clusterProps: {
+        replicationSpecs: replicationSpecs,
+      },
+      projectProps: {
+        orgId: orgId,
+      },
+    });
   }
 }
+
+
+
+const replicationSpecs = [
+  {
+    numShards: 1,
+    advancedRegionConfigs: [
+      {
+        analyticsSpecs: {
+          ebsVolumeType: 'STANDARD',
+          instanceSize: 'M10',
+          nodeCount: 1,
+        },
+        electableSpecs: {
+          ebsVolumeType: 'STANDARD',
+          instanceSize: 'M10',
+          nodeCount: 3,
+        },
+        priority: 7,
+        regionName: 'US_EAST_1',
+      },
+    ],
+  },
+];
 
 // for development, use account/region from cdk cli
 const devEnv = {
